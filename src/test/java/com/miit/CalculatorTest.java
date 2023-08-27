911996f25d6a5759b21a1dae1c7250dd5638343b
khushbu patel <khushbu.patel061997@gmail.com>
2023-08-27 10:42:43 -0400

    my initial commit

---
+++ src/test/java/com/miit/CalculatorTest.java
@@ -0,0 +1,27 @@
+package com.miit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+	
+//	@Test
+//	public void addTest() {
+//		Calculator calculator = new Calculator();
+//		
+//		int result = calculator.add(5,4);
+//		int actual = 9;
+//		
+//		assertEquals(result,actual);
+//	
+//		
+//		
+//	}
+	
+	
+	
+		
+		
+	}
+	
